6fb8eb282094a3e7c60458a99d89667fe5cacce4
itc-s24001 <s24001@std.it-college.ac.jp>
2024-07-04 07:52:33 +0000

    24/07/04 2-5~2-9

---
+++ 2-6.py
@@ -0,0 +1,17 @@
+a = "abc"
+b = "xyz"
+print("{} {}".format(a, b))
+
+title1 = "name"
+value1 = "John Smith"
+print("Your {} is {}.".format(title1, value1))
+
+age = 20
+print("Your age is {}.".format(age))
+
+title2 = "IQ"
+value2 = 100
+print("{} = {}".format(title2, value2))
+
+a, b = "Hello", "World"
+print(f"{a} {b}")
